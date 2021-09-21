e425f4d60f8c15f36fe6c79949d223ecd682422f
Eva <mac@macs-MacBook-Pro.local>
2021-09-21 09:36:43 -0300

    coors

--- index.js
+++ index.js
@@ -9,22 +9,23 @@ const app =  express();
 //conectar la base de datos
 conectarDB();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
 //habilitar cors
-// app.use(cors());
-
-
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+app.use(cors());
 
 //habilitar exoress.json
 app.use(express.json({extended: true }));
-
 
 //puerto de la app servidor
 const port = process.env.port || 4000;
@@ -34,12 +35,6 @@ app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
-
-
-//definir la pag principa
-//app.get('/', (req, res)=>{
-//   res.send('hola mundo')
-//});
 
 // arrancar la app
 app.listen (port,'0.0.0.0', () => {
